452129d58bfb2bc4445436444aaad721e68e41e0
jackie <jcqnly@gmail.com>
2018-02-09 14:45:58 -0800

    add q4 and it pass

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ function sumAndMultiply(a, b, c) { //eslint-disable-line
   var theMultiple = a * b * c;
   var thirdMessage = (a + ' and ' + b + ' and ' + c + ' sum to ' + theSum + '.');
   var fourthMessage = ('The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + theMultiple + '.');
-  return[theSum, theMultiple, thirdMessage, fourthMessage];
+  return [theSum, theMultiple, thirdMessage, fourthMessage];
 }
 
 // TODO: Here is the test for sumAndMultiply(); uncomment it to run it
@@ -81,11 +81,13 @@ Test this function by hand in the console to get it working, and when you think 
 var testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(testArray) { //eslint-disable-line
-
+  var arraySum = testArray[0] + testArray[1] + testArray[2];
+  var message = testArray[0] + ',' + testArray[1] + ',' + testArray[2] + ' was passed in as an array of numbers, and ' + arraySum + ' is their sum.';
+  return [arraySum, message];
 }
 
 // TODO: Here is the test for sumArray(); uncomment it to run it
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
